3d74987a552dce3a299c16a4937490662d9ae0cc
Maninder <BeatDevTeam@gmail.com>
2018-11-04 12:41:17 +0530

    Maninder : Added Meta data DLL and Created Demo Class Structure

---
+++ Rhythm/MetaData/Class1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetaData
+{
+    public class Subject : ISubject
+    {
+        public Subject GetSubject(int subjectId)
+        {
+            return null;
+        }
+    }
+    public class Chapter : IChapter
+    {
+        public Chapter[] GetChapters(int subjectId)
+        {
+            return null;
+        }
+
+    }
+    public class Topic : ITopic
+    {
+        public Topic[] GetTopics(int chapterId)
+        {
+            return null;
+        }
+
+    }
+    public class Subtopic : ISubtopic
+    {
+        public Subtopic[] GetSubTopics(int subtopicId)
+        {
+            return null;
+        }
+    }
+    public class SubtopicDescription : ISubtopicDescription
+    {
+        public SubtopicDescription[] GetSubDescription(int subtopicId)
+        {
+            return null;
+        }
+    }
+    public class TopicDesrciption : ITopic
+    {
+    }
+    public class Script : IScript
+    {
+    }
+
+
+    public interface ISubject
+    {
+        Subject GetSubject(int subjectId);
+    }
+    public interface IChapter
+    {
+    }
+    public interface ITopic
+    {
+    }
+    public interface ISubtopic
+    {
+    }
+    public interface ISubtopicDescription
+    {
+    }
+    public interface ITopicDesrciption
+    {
+    }
+    public interface IScript
+    {
+    }
+
+}
